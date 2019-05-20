abb83d8294ab6167e955555f77013b7235fc2e6f
obiwan1337 <andre.schaefer@hs-furtwangen.de>
2019-05-20 20:50:09 +0900

    i > 0 alone for root

--- FreeMindView2/testing/ts/main.ts
+++ FreeMindView2/testing/ts/main.ts
@@ -234,7 +234,7 @@ namespace Freemindtesting {
         if (ctx.isPointInPath(fmvNodes[i].pfadrect, _event.clientX, _event.clientY)) {
 
           fmvNodes[i].folded = !fmvNodes[i].folded;
-          if (i > 0 && fmvNodes[i].parent.mapPosition == "root") {
+          if (i > 0 /* && fmvNodes[i].parent.mapPosition == "root" */) {
             console.log("boi i shuld be false");
             fmvNodes[i].folded = false;
           }
